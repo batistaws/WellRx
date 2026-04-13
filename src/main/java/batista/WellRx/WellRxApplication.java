a9ba918d8d7300e8aba74e276539bd2209332caf
DevBatista <batijbatista@gmail.com>
2026-04-13 20:19:17 -0300

    projeto inicial

---
+++ src/main/java/batista/WellRx/WellRxApplication.java
@@ -0,0 +1,13 @@
+package batista.WellRx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellRxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellRxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batista/WellRx/WellRxApplicationTests.java
@@ -0,0 +1,13 @@
+package batista.WellRx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellRxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
